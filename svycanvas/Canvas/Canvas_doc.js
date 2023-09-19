13fe01032e5a5039701fa773fad52f00dfbffb07
Johan Compagner <jcompagner@servoy.com>
2023-09-19 15:56:01 +0200

    created doc file (but no doc on all the js functions)

---
+++ svycanvas/Canvas/Canvas_doc.js
@@ -0,0 +1,35 @@
+function bringToFront(idx) {
+}
+
+function copySelectedObject() {
+}
+
+function saveAsImage(cb) {
+}
+
+function saveCanvas(cb) {
+}
+function ZoomOnPoint(x, y, zoom) {
+}
+function updateObject(obj, setItemActive) {
+}
+function loadCanvas(data) {
+}
+function addObject(objs, setActive) {
+}
+function removeObject(idx) {
+}
+function clearCanvas() {
+}
+function setSelectedObject(ids) {
+}
+function getSelectedObject(cb, sel) {
+}
+function draw() {
+}
+function rotate(angle) {
+}
+function startAnimate() {
+}
+function stopAnimate() {
+}
