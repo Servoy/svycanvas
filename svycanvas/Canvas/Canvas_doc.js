ee3aa3432628eb1b1e85828d3c49a0b90c52beb8
mvid <mvid@servoy.com>
2025-01-30 18:36:54 +0200

    SVY-19830 Review and update all _doc.js files from documented plugins

--- svycanvas/Canvas/Canvas_doc.js
+++ svycanvas/Canvas/Canvas_doc.js
@@ -1,9 +1,9 @@
 /**
  * Brings a specific element given it's ID, to the front layer of the canvas 
  * 
- * @param {string} idx
+ * @param {String} id The ID of the element to bring to the front layer of the canvas.
  */
-function bringToFront(idx) {
+function bringToFront(id) {
 }
 
 /**
@@ -17,7 +17,7 @@ function copySelectedObject() {
  * The callback has a single parameter 
  * which contains the base64 data of the canvas.  
  * 
- * @param {Function} imgCB
+ * @param {Function} imgCB A callback function that receives the base64-encoded image data of the canvas.
  */
 function saveAsImage(imgCB) {
 }
@@ -26,38 +26,40 @@ function saveAsImage(imgCB) {
  * Save the canvas as a JSON object.  
  * The callback has a single parameter which contains the JSON object.   
  * This JSON object can be used with the loadCanvas method.
- * @param {Function} saveCB
+ * 
+ * @param {Function} saveCB A callback function that receives the serialized JSON object representing the canvas.
  */
 function saveCanvas(saveCB) {
 }
 
 /**
  * Zooms into the canvas given the x/y coords and a zoom level.  
- * @param {Number} x
- * @param {Number} y
- * @param {Number} zoom
+ * @param {Number} x The x-coordinate to center the zoom on.
+ * @param {Number} y The y-coordinate to center the zoom on.
+ * @param {Number} zoom The zoom level to apply to the canvas.
  */
 function ZoomOnPoint(x, y, zoom) {
 }
 
 /**
  * Update a specific canvas object and optionally select it  
- * @param {Object} obj 
- * @param {Boolean} setItemActive
+ * @param {Object} obj The canvas object to update with new properties or modifications.
+ * @param {Boolean} selectActiveItems If true, the updated object will be selected as the active item.
  */
-function updateObject(obj, setItemActive) {
+function updateObject(obj, selectActiveItems) {
 }
 
 /**
  * Loads the canvas from a JSON object  
- * @param {Object} data
+ * @param {String} data The JSON string containing the canvas structure and properties to be loaded.
  */
 function loadCanvas(data) {
 }
 
 /**
  * Print the current canvas view (TING only).  Can use an optional DPI resolution for higher quality prints.
- * @param {Number} resolutionWidth  
+ * 
+ * @param {Number} resolutionWidth The optional DPI resolution width for higher quality prints.
  */
 function printCanvas(resolutionWidth) {
 }
@@ -65,17 +67,19 @@ function printCanvas(resolutionWidth) {
 /**
  * Add one or more canvas objects and optionally select it. 
  * If given more than one item in the objs array, the elements will be grouped as a single entity. 
- * @param {Array<Object>} objs 
- * @param {Boolean} setActive
+ * 
+ * @param {Array<Object>} objs An array of canvas objects to be added.
+ * @param {Boolean} [setActive] If true, the added object(s) will be selected as the active item.
  */
 function addObject(objs, setActive) {
 }
 
 /**
  * Remove an canvas object given it's ID.
- * @param {String} idx
+ * 
+ * @param {String} id The ID of the canvas object to be removed.
  */
-function removeObject(idx) {
+function removeObject(id) {
 }
 
 /**
@@ -86,21 +90,24 @@ function clearCanvas() {
 
 /**
  * Select one or more objects in the canvas given it's ID.
- * @param {Array<String>} ids 
+ * 
+ * @param {Array<String>} ids An array of object IDs to be selected on the canvas.
  */
 function setSelectedObject(ids) {
 }
 
 /**
  * Get the selected object
- * @param {Function} cb 
+ * 
+ * @param {Function} saveCB A callback function that receives the selected canvas object.
  */
 function getSelectedObject(cb) {
 }
 
 /**
  * Rotate the canvas at a specific angle
- * @param {Number} angle 
+ * 
+ * @param {Number} angle The angle, in degrees, to rotate the canvas.
  */
 function rotate(angle) {
 }
