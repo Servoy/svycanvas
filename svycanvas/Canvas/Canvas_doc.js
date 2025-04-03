2c067ddee8e865cf3e8c90034b7f9ffc4b47b07e
mvid <mvid@servoy.com>
2025-04-03 10:08:17 +0300

    SVY-19926 SpecMarkdownGenerator - incomplete/missing documentation for handlers

--- svycanvas/Canvas/Canvas_doc.js
+++ svycanvas/Canvas/Canvas_doc.js
@@ -1,27 +1,62 @@
+/**
+ * Array of canvas objects to be rendered on the canvas.
+ */
 var canvasObjects;
 
+/**
+ * Flag to indicate whether the grid background should be displayed.
+ */
 var showGrid;
 
+/**
+ * Flag to enable snapping canvas objects to the grid when moved or resized.
+ */
 var snapToGrid;
 
+/**
+ * The size, in pixels, of each grid cell used for drawing and snapping.
+ */
 var gridSize;
 
+/**
+ * Configuration options for the canvas rendering engine.
+ */
 var canvasOptions;
 
+/**
+ * Array of media references for images that are preloaded for use in canvas objects.
+ */
 var imagesLoader;
 
+/**
+ * CSS class applied to the canvas container for custom styling.
+ */
 var styleClass;
 
-
 var handlers = {
+    /**
+     * Called when a canvas object is clicked.
+     */
     onClick: function() {},
 
+    /**
+     * Called when a canvas object is long-pressed (touch event).
+     */
     onLongPress: function() {},
 
+    /**
+     * Called when a canvas object is modified (e.g., moved, scaled, or rotated).
+     */
     onModified: function() {},
 
+    /**
+     * Called when the canvas is fully initialized and ready for interaction.
+     */
     onReady: function() {},
 
+    /**
+     * Called after the canvas has finished rendering all objects.
+     */
     afterRender: function() {}
 };
 
@@ -153,99 +188,234 @@ function stopAnimate() {
 
 var svy_types = {
 
+    /**
+     * Configuration options for the canvas.
+     */
     canvasOptions: {
+        /**
+         * Determines if canvas objects are selectable.
+         */
+        selectable: null,
 
-        selectable : null,
+        /**
+         * If true, disables target finding for canvas objects to improve performance.
+         */
+        skipTargetFind: null,
 
-        skipTargetFind : null,
+        /**
+         * Indicates whether canvas objects display a rotation control handle.
+         */
+        hasRotatingPoint: null,
 
-        hasRotatingPoint : null,
+        /**
+         * Specifies if the canvas should automatically re-render when objects are added or removed.
+         */
+        renderOnAddRemove: null,
 
-        renderOnAddRemove : null,
+        /**
+         * If true, objects outside the visible canvas area are not rendered.
+         */
+        skipOffscreen: null,
 
-        skipOffscreen : null,
+        /**
+         * Enables zooming via mouse scroll when true.
+         */
+        ZoomOnMouseScroll: null,
 
-        ZoomOnMouseScroll : null,
-
-        animationSpeed : null,
-
+        /**
+         * Sets the speed of canvas animations in milliseconds.
+         */
+        animationSpeed: null,
     },
 
+
+    /**
+     * Definition of a canvas object.
+     */
     canvasObject: {
+        /**
+         * The type of canvas object (e.g., 'Circle', 'Rect', 'Triangle', etc.).
+         */
+        objectType: null,
 
-        objectType : null,
+        /**
+         * For grouped objects, holds an array of child objects.
+         */
+        objects: null,
 
-        objects : null,
+        /**
+         * The fill color of the canvas object.
+         */
+        fill: null,
 
-        fill : null,
+        /**
+         * The opacity level of the canvas object.
+         */
+        opacity: null,
 
-        opacity : null,
+        /**
+         * The width of the canvas object.
+         */
+        width: null,
 
-        width : null,
+        /**
+         * The height of the canvas object.
+         */
+        height: null,
 
-        height : null,
+        /**
+         * For circular objects, the radius.
+         */
+        radius: null,
 
-        radius : null,
+        /**
+         * The x-coordinate position of the canvas object.
+         */
+        left: null,
 
-        left : null,
+        /**
+         * The y-coordinate position of the canvas object.
+         */
+        top: null,
 
-        top : null,
+        /**
+         * The horizontal corner radius for rounded rectangles.
+         */
+        rx: null,
 
-        rx : null,
+        /**
+         * The vertical corner radius for rounded rectangles.
+         */
+        ry: null,
 
-        ry : null,
+        /**
+         * The stroke color of the canvas object.
+         */
+        stroke: null,
 
-        stroke : null,
+        /**
+         * The width of the stroke (outline) of the canvas object.
+         */
+        strokeWidth: null,
 
-        strokeWidth : null,
+        /**
+         * The path data for path-based objects.
+         */
+        path: null,
 
-        path : null,
+        /**
+         * Array of points defining a polygon or polyline.
+         */
+        points: null,
 
-        points : null,
+        /**
+         * A unique identifier for the canvas object.
+         */
+        id: null,
 
-        id : null,
+        /**
+         * The horizontal scaling factor for the object.
+         */
+        scaleX: null,
 
-        scaleX : null,
+        /**
+         * The vertical scaling factor for the object.
+         */
+        scaleY: null,
 
-        scaleY : null,
+        /**
+         * For text objects, the text content.
+         */
+        text: null,
 
-        text : null,
+        /**
+         * For text objects, the text alignment (e.g., left, center, right).
+         */
+        textAlign: null,
 
-        textAlign : null,
+        /**
+         * For text objects, the font size.
+         */
+        fontSize: null,
 
-        fontSize : null,
+        /**
+         * For text objects, the font family.
+         */
+        fontFamily: null,
 
-        fontFamily : null,
+        /**
+         * The rotation angle of the canvas object in degrees.
+         */
+        angle: null,
 
-        angle : null,
+        /**
+         * The style of the line join for the stroke (e.g., miter, round, bevel).
+         */
+        strokeLineJoin: null,
 
-        strokeLineJoin : null,
+        /**
+         * For image objects, the name of the media resource.
+         */
+        mediaName: null,
 
-        mediaName : null,
+        /**
+         * For sprite objects, the name of the sprite sheet.
+         */
+        spriteName: null,
 
-        spriteName : null,
+        /**
+         * The width of the sprite frame.
+         */
+        spriteWidth: null,
 
-        spriteWidth : null,
+        /**
+         * The height of the sprite frame.
+         */
+        spriteHeight: null,
 
-        spriteHeight : null,
+        /**
+         * The current index of the sprite frame.
+         */
+        spriteIndex: null,
 
-        spriteIndex : null,
+        /**
+         * The time (in milliseconds) for each frame in sprite animation.
+         */
+        frameTime: null,
 
-        frameTime : null,
+        /**
+         * If true, the object is flipped horizontally.
+         */
+        flipX: null,
 
-        flipX : null,
+        /**
+         * If true, the object is flipped vertically.
+         */
+        flipY: null,
 
-        flipY : null,
+        /**
+         * Determines if the object is selectable.
+         */
+        selectable: null,
 
-        selectable : null,
+        /**
+         * Holds control data for the object.
+         */
+        ctrl: null,
 
-        ctrl : null,
+        /**
+         * Contains state information for the canvas object.
+         */
+        state: null,
 
-        state : null,
+        /**
+         * An auxiliary property for state management.
+         */
+        stateHolder: null,
 
-        stateHolder : null,
-
-        custom_data : null,
-
+        /**
+         * A generic property to store custom data related to the canvas object.
+         */
+        custom_data: null,
     }
 }
